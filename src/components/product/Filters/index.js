e5c8ffaadff654d444e082f151cc5df4e86d9182
Dmitrij.Shlanchak <d.shlanchak@gmail.com>
2025-02-15 12:23:57 +0100

    add page-dependent filter rendering

--- src/components/product/Filters/index.js
+++ src/components/product/Filters/index.js
@@ -1,21 +1,27 @@
 import Input from '../../common/Input';
 import s from './index.module.css';
 import Spoiler from '../../common/Spoiler';
+import { useLocation } from 'react-router-dom';
 
-export default function Filters
-() {
+export default function Filters () {
+  const location = useLocation();
+
   return (
     <Spoiler title="Filter">
       <div className={s.container}>
         <div className={s.row}>
-            <div className={s.price}>
-                <Input type={'text'} placeholder='from' borderColor="green" label={'Price'}/>
-                <Input type='text' placeholder='to'/>
-            </div>
-            <Input type={'radio' } label={'Discounted items'}/>
-            <Input type={'text'} placeholder='default' label={'Sorted'}/>
+          <div className={s.price}>
+            <Input type={'text'} placeholder='from' borderColor="green" label={'Price'}/>
+            <Input type='text' placeholder='to'/>
+          </div>
+
+          {location.pathname !== '/sale' && (
+            <Input type={'radio'} label={'Discounted items'} />
+          )}
+
+          <Input type={'text'} placeholder='default' label={'Sorted'}/>
         </div>
-     </div>
+      </div>
     </Spoiler>
     
   )
--- src/pages/Sale.jsx
+++ src/pages/Sale.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Filters from '../components/product/Filters'
 
 export default function Sale() {
   return (
     <div>
       <h2>Sale</h2>
+      <Filters/>
     </div>
   )
 }
